d71906ae26622d0bc79b80d01496e13c1873bc7f
pengfei.huang <huangpengfei@qudian.com>
2024-03-08 10:06:45 +0800

    FIX ignore kbar search bug

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -81,7 +81,7 @@ const siteMetadata = {
   search: {
     provider: 'kbar', // kbar or algolia
     kbarConfig: {
-      searchDocumentsPath: 'me/search.json', // path to load documents to search
+      searchDocumentsPath: 'search.json', // path to load documents to search
     },
     // provider: 'algolia',
     // algoliaConfig: {
